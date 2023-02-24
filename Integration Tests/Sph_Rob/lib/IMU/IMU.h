0a8e47ca8a72d15601fbf859e3cecb0b79756ad8
Tommy Le <TaiLe2435@gmail.com>
2023-02-24 17:23:05 -0600

    Created src and .h file for IMU and fixed bug in QuickI2C

---
+++ Integration Tests/Sph_Rob/lib/IMU/IMU.h
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+void transform(float accX, float accY, float accZ, float matrix[3][3], String acc );
+
+void transpose(float matrix[3][3]);
+
+void euler(int sDdot[3][1], int sdot[3][1], int s0[3][1]);
+
+void simpsons();
+
+void trapezoidal(int s1[3][1], int s0[3][1], int ic[3][1]);
+
+unsigned long CalculateDeltaTime();
+
+void initIMU();
+
+void poseEstimation(); 
---
+++ Integration Tests/Sph_Rob/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "IMU.h"
+
+void setup() {
+  Serial.begin(115200);
+  initIMU();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
