2b7ebafbf80d0811829cef12ffc9bfe9cf126198
Tommy Le <TaiLe2435@gmail.com>
2023-03-02 13:59:43 -0600

    added P control

---
+++ Integration Tests/Sph_Rob/lib/robotControl/robotControl.h
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void BTinit();
+
+void BTmove();
+
+void move(int uR, int uL);
+
+void stop();
+
+int* circle(float radius);
--- Integration Tests/Sph_Rob/src/main.cpp
+++ Integration Tests/Sph_Rob/src/main.cpp
@@ -1,13 +1,27 @@
 #include <Arduino.h>
 #include "IMU.h"
+#include "robotControl.h"
 
 void setup() {
   Serial.begin(115200);
   initIMU();
+  BTinit();
 }
 
 void loop() {
-  float heading = 0;
-  heading = poseEstimation();
-  Serial.println(heading);
+  float desired = 0; // is going to be coming from python program
+  float kP = 0.2;
+  bool flag = true;
+  float heading = poseEstimation();
+  // Serial.println(heading);
+
+  // P control
+  if(flag) // py will set flag 1 until robot gets to target pos
+  {
+    float error = desired - heading;
+    float leftWheel = 100 + kP*error;
+    float rightWheel = 100 - kP*error;
+    move(leftWheel, rightWheel);
+  }
+
 }
